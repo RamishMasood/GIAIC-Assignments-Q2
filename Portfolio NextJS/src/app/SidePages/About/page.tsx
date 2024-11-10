0d75bc316133c163f52ba3a7d05073ffe581fe31
Ramish Masood <ramishmasood3@gmail.com>
2024-11-10 20:11:17 +0500

    update

---
+++ Portfolio NextJS/src/app/SidePages/About/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function About() {
+  return (
+    <div className="right-side about">
+      <h1>About Me</h1>
+      <p>I'm Adil, a passionate computer engineer with a drive to innovate and create impactful software solutions. With expertise in multiple programming languages, I craft technology that simplifies lives. Let's connect and explore possibilities.
+      </p>
+    </div>
+  );
+}
