ed0d9cffc5ee49a68a70864e59f793b106f8fbdf
Ramish Masood <ramishmasood3@gmail.com>
2024-11-10 21:48:39 +0500

    update

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,9 +2,8 @@ export default function Home() {
   return (
     <div className="right-side home" >
       <div>
-        <h1>Adil Aijaz</h1>
-        <p>House No. A-23, Pak Ideal Cooperative Housing Society, Gate No.1, Scheme 33, Sector 39-A, Malir Cantt. Karachi-75070.</p>
-        
+        <h1>Ramish Masood</h1>
+        <p>Computer Engineer</p>
       </div>
     </div>
   );
