607c9227df098e148a5c67a213bd22d0212d6d25
Ramish Masood <ramishmasood3@gmail.com>
2024-12-09 01:35:47 +0500

    update

--- test/src/app/page.tsx
+++ test/src/app/page.tsx
@@ -1,21 +1,24 @@
-import Navbar from "@/components/home/navbar";
-import Hero from "@/components/home/hero";
-import Bestseller from "@/components/home/bestseller";
-import Product from "@/components/home/product";
-import Buynow from "@/components/home/buynow";
-import Featured from "@/components/home/featured";
-import Footer from "@/components/home/footer";
+import BlueHeader from "./components/blue-header";
+import Carausel from "./components/caraousel";
+import Editors from "./components/editorspick";
+import Footer from "./components/footer";
+import GreenDiv from "./components/green-div";
+import Navbar from "./components/navbar";
+import ProductCard from "./components/products-card";
+import Whitediv from "./components/white-dic";
 
-export default function Home() {
-  return (
+export default function Home(){
+  return(
     <div>
-      <Navbar />
-      <Hero />
-      <Bestseller />
-      <Product />
-      <Buynow />
-      <Featured />
-      <Footer />
+      <BlueHeader/>
+      <Navbar/>
+      <Carausel/>
+      <Editors/>
+      <ProductCard/>
+      <GreenDiv/>
+      <Whitediv/>
+      <Footer/>
+           
     </div>
-  );
+  )
 }
