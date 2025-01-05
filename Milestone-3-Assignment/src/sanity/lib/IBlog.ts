d590493bea26d43c109322a9f2b1653fd146f033
Ramish Masood <ramishmasood3@gmail.com>
2025-01-05 22:24:09 +0500

    update

---
+++ Milestone-3-Assignment/src/sanity/lib/IBlog.ts
@@ -0,0 +1,18 @@
+
+
+export default interface IBlog {
+  title: string;
+  category: string;
+  author: string;
+  desc: string;
+  _id: string;
+  excerpt: string;
+  date_posted: string;
+  image: {
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+    _type: string;
+  };
+}
